89f6e05c2a7adb81e952011be2d5a432a9a4ac8d
Everton Cabral <evertoncab@gmail.com>
2021-02-27 23:08:22 -0300

    criando funções JS para as barreiras

---
+++ js/flappy.js
@@ -0,0 +1,49 @@
+function novoElemento(tagName, className) {
+  const elem = document.createElement(tagName);
+  elem.className = className;
+  return elem;
+}
+
+function barreira(reversa = false) {
+  this.elemento = novoElemento("div", "barreira");
+  const borda = novoElemento("div", "borda");
+  const corpo = novoElemento("div", "corpo");
+
+  this.elemento.appendChild(reversa ? corpo : borda);
+  this.elemento.appendChild(reversa ? borda : corpo);
+
+  this.setAltura = (altura) => (corpo.style.height = `${altura}px`);
+}
+
+// const b = new barreira(true);
+// b.setAltura(200);
+// document.querySelector("[tp-flappy]").appendChild(b.elemento);
+
+function parDeBarreiras(altura, abertura, posicao) {
+  this.elemento = novoElemento("div", "par-de-barreiras");
+
+  this.superior = new barreira(true);
+  this.inferior = new barreira(false);
+
+  this.elemento.appendChild(this.superior.elemento);
+  this.elemento.appendChild(this.inferior.elemento);
+
+  this.sortearAbertura = () => {
+    const alturaSuperior = Math.random() * (altura - abertura);
+    const alturaInferior = altura - abertura - alturaSuperior;
+    this.superior.setAltura(alturaSuperior);
+    this.inferior.setAltura(alturaInferior);
+  };
+
+  this.getPosicao = () => parseInt(this.elemento.style.left.split("px")[0]);
+
+  this.setPosicao = (posicao) => (this.elemento.style.left = `${posicao}px`);
+
+  this.getLargura = () => this.elemento.clientWidth;
+
+  this.sortearAbertura();
+  this.setPosicao(posicao);
+}
+
+const b = new parDeBarreiras(700, 200, 800);
+document.querySelector("[tp-flappy]").appendChild(b.elemento);
